36fe792a49e696b701a7deafec551dcb6f2f0dfd
DarkSoldierBR <mateus.dias029@gmail.com>
2020-04-15 13:37:43 -0300

    improved appbar alignment

--- lib/view/common/appbar.dart
+++ lib/view/common/appbar.dart
@@ -1,34 +1,27 @@
 import 'package:flutter/material.dart';
 
 Widget buildAppBar(context) {
-  return PreferredSize(
-      preferredSize: Size.fromHeight(70.0),
-      child: AppBar(
-        //  iconTheme: new IconThemeData(size: 15.0,color: Colors.black54),
-        //         leading: IconButton(
-        //           icon: Icon(Icons.dehaze),
-        //           color: Colors.black54,
-        //           iconSize: 40.0,
-        //             onPressed: () {
-        //  //     Scaffold.of(context).openDrawer();
-
-        //     },
-
-        leading: Builder(builder: (BuildContext context) {
-          return IconButton(
-              icon: Icon(Icons.dehaze),
-              color: Colors.black54,
-              iconSize: 40.0,
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              });
-        }),
-
-        title: Row(
-          verticalDirection: VerticalDirection.down,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Image.asset("assets/logo/logo-horizontal.png")],
-        ),
-        backgroundColor: Color.fromRGBO(192, 192, 192, 1.0),
-      ));
+  return AppBar(
+    leading: Builder(builder: (BuildContext context) {
+      return IconButton(
+          icon: Icon(Icons.dehaze),
+          color: Colors.black54,
+          iconSize: 40.0,
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          });
+    }),
+    title: Center(
+        child: Row(
+      verticalDirection: VerticalDirection.down,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          "assets/logo/logo-horizontal.png",
+          width: 180,
+        )
+      ],
+    )),
+    backgroundColor: Color.fromRGBO(192, 192, 192, 1.0),
+  );
 }
