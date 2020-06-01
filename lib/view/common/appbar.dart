274b4f51c5a645bc4e086f9f21c72c36c2e97a2f
DarkSoldierBR <mateus.dias029@gmail.com>
2020-06-01 11:21:08 -0300

    improve appbar #1

--- lib/view/common/appbar.dart
+++ lib/view/common/appbar.dart
@@ -3,28 +3,28 @@ import 'package:flutter/material.dart';
 Widget buildAppBar(context) {
   return AppBar(
     leading: Builder(builder: (BuildContext context) {
-      return Container(color:Colors.blue,
-        child:IconButton(
-          icon: new Image.asset("assets/logo/menu-icone.png"),
-          color: Colors.black54,
-          iconSize: 40.0,
-          onPressed: () {
-            Scaffold.of(context).openDrawer();
-          }));
+      return Container(
+          color: Colors.blue,
+          child: IconButton(
+              icon: new Image.asset("assets/logo/menu-icone.png"),
+              iconSize: 50.0,
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              }));
     }),
-    title: Center(
-        child: Container(color:Colors.red,
-        child:
-        Row(
-      verticalDirection: VerticalDirection.down,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset(
-          "assets/logo/logo-horizontal.png",
-          width: 250,
-        )
-      ],
-    ))),
+    title: Container(
+        color: Colors.red,
+        width: double.infinity,
+        child: Row(
+          verticalDirection: VerticalDirection.down,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/logo/logo-horizontal.png",
+              width: 250,
+            )
+          ],
+        )),
     backgroundColor: Color.fromRGBO(192, 192, 192, 1.0),
   );
 }
