92fca858374d027eba1515bcd3a2c6a41b47e983
DarkSoldierBR <mateus.dias029@gmail.com>
2020-06-01 09:54:25 -0300

    agroup task and title

--- lib/view/screens/home/home.dart
+++ lib/view/screens/home/home.dart
@@ -12,7 +12,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.black,
         drawer: buildDrawer(context),
         appBar: buildAppBar(context),
         body: Container(
@@ -36,26 +36,60 @@ class _HomeState extends State<Home> {
                           fontWeight: FontWeight.bold),
                     ),
                   )),
-              SizedBox(
-                  width: double.infinity,
-                  // height: double.infinity,
-                  child: Container(
-                      color: Color.fromRGBO(128, 128, 128, 1.0),
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Tarefas",
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',                            
-                            fontSize: 30.0,
-                            color: Color.fromRGBO(220, 220, 220, 1.0),
-                            fontWeight: FontWeight.bold),
-                      ))),
-              Column(
-                children: <Widget>[
-                  buildTask("9:00", "Acordar"),
-                  buildTask("22:00", "Dormir"),
-                  buildTask("13:00", "Estudar"),
-                ],
+
+              // Container(
+
+              //     decoration: BoxDecoration(
+              //       border: Border.all(width: 3.0,color: ),
+              //       borderRadius: BorderRadius.all(
+              //         Radius.circular(25),
+              //       ),
+              //       //  color: Color.fromRGBO(128, 128, 128, 1) //cinza forte
+              //       color: Colors.deepPurpleAccent,
+              //     ),
+              //     margin: EdgeInsets.all(5),
+              //     padding: EdgeInsets.all(7),
+              //     child:
+
+              Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 3.0,color: Colors.blueAccent),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25),
+                    ),
+                    //  color: Color.fromRGBO(128, 128, 128, 1) //cinza forte
+                   // color: 
+                  ),
+                //color: Colors.red,
+
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 150,
+                        margin: EdgeInsets.all(5),
+                        padding: EdgeInsets.all(7),
+                        color: Color.fromRGBO(128, 128, 128, 1.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Tarefas",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 30.0,
+                              color: Color.fromRGBO(220, 220, 220, 1.0),
+                              fontWeight: FontWeight.bold),
+                        )),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      color: Colors.greenAccent,
+                        child: Column(
+                      children: <Widget>[
+                        buildTask("9:00", "Acordar"),
+                        buildTask("22:00", "Dormir"),
+                        buildTask("13:00", "Estudar"),
+                      ],
+                    ))
+                  ],
+                ),
               ),
             ],
           ),
